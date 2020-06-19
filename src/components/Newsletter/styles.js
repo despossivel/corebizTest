2dec23513f15a55e847d2c507757b7786d492a69
despossivel <mattbmoller@gmail.com>
2020-06-19 16:39:44 -0300

    slider

--- src/components/Newsletter/styles.js
+++ src/components/Newsletter/styles.js
@@ -7,6 +7,12 @@ export const Container = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
+
+    @media  (max-width: 700px)
+    {
+        height: 30vh;
+    }
+
 `;
 
 export const Text = styled.div`
@@ -29,4 +35,21 @@ export const FlexContainer = styled.div`
         margin-left: 15px;
         margin-right: 15px;
     }
+
+    @media  (max-width: 700px)
+    {
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+
+        div+div{
+            margin-top: 15px;
+            margin-bottom: 15px;
+        }
+
+
+    }
+
+
 `;
--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,9 +1,48 @@
 import React, { memo } from 'react';
+import SmartSlider from "react-smart-slider";
 
-import { Container } from './styles';
+import BannerBlack from '../../asserts/bannerBlack.png';
+import Banner from '../../asserts/banner.png';
+
+import {
+  Container,
+  ContainerImgs,
+  ContainerText,
+  Children,
+  Text,
+  BannerBackground,
+  BannerImg,
+  AlingText
+} from './styles';
 
 const Slider = () => {
-  return <Container>Slider</Container>;
+  return <Container>
+
+    <SmartSlider
+      slides={[
+        {
+          // url: Banner,
+          childrenElem: <Children>
+            <ContainerText>
+              <AlingText>
+                <Text color="#fff" size="14px" >Olá, o que você está buscando?</Text>
+                <Text fontWeight="bold" color="#fff" size="33px" >Criar ou migrar seu e-commerce?</Text>
+              </AlingText>
+            </ContainerText>
+            <ContainerImgs>
+              <BannerBackground src={BannerBlack} alt="w" />
+              <BannerImg src={Banner} alt="dsf" />
+            </ContainerImgs>
+          </Children>
+        },
+        {
+          url: Banner
+        }
+      ]}
+      buttonShape="square" // round or square
+    />
+
+  </Container>;
 }
 
 export default memo(Slider);
