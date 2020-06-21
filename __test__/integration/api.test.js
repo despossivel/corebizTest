f22d3c5d43e727846c7ea125218da73c282fe992
despossivel <mattbmoller@gmail.com>
2020-06-21 12:03:25 -0300

    refatorando testes

--- __test__/integration/api.test.js
+++ __test__/integration/api.test.js
@@ -2,9 +2,13 @@ import request from 'supertest';
 
 const api = request('https://corebiz-test.herokuapp.com/api/v1');
 
-test('Get products', () => api.get('/products').expect(200));
+describe('API - Integração', () => {
 
-test('Add newsletter', () => api.post('/newsletter').send({
-    name: 'Matheus',
-    email: 'mattbmoller@gmail.com'
-}).expect(200));
+    test('Get products', () => api.get('/products').expect(200));
+
+    test('Add newsletter', () => api.post('/newsletter').send({
+        name: 'Matheus',
+        email: 'mattbmoller@gmail.com'
+    }).expect(200));
+
+});
